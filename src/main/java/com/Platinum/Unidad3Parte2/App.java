83b4960f72829b594536a0beb0d7e8fa8f33335b
soybecky <88678346+soybecky@users.noreply.github.com>
2021-09-06 19:34:28 -0300

    Add files via upload

---
+++ src/main/java/com/Platinum/Unidad3Parte2/App.java
@@ -0,0 +1,13 @@
+package com.Platinum.Unidad3Parte2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola, Que tal?" );
+    }
+}
